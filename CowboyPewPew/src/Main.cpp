80a3160d5eb8049f09b459a1d61448956c627cda
rhv9 <rahulhasmukv@gmail.com>
2024-03-30 13:28:53 +0000

    Linked GLFW and Glad to project.

--- CowboyPewPew/src/Main.cpp
+++ CowboyPewPew/src/Main.cpp
@@ -1,9 +1,43 @@
 #include "pch.h"
 
 #include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
+void error_callback(int error, const char* description)
+{
+	fprintf(stderr, "GLFW Error: %s\n", description);
+}
 
 int main()
 {
 	std::cout << "It begins now!" << std::endl;
+	if (!glfwInit())
+	{
+		return -1;
+	}
+
+	
+
+	glfwSetErrorCallback(error_callback);
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Title", NULL, NULL);
+	if (!window)
+	{
+		std::cerr << "Failed to initialize window!" << std::endl;
+	}
+	glfwMakeContextCurrent(window);
+
+	gladLoadGL();
+
+	while (!glfwWindowShouldClose(window))
+	{
+		// Keep running
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	
 }
--- premake5.lua
+++ premake5.lua
@@ -14,7 +14,15 @@ workspace "CowboyPewPew"
     }
 
 
-Outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+IncludeDir = {}
+IncludeDir["GLFW"] = "CowboyPewPew/vendor/GLFW/include"
+IncludeDir["Glad"] = "CowboyPewPew/vendor/Glad/include"
+IncludeDir["ImGui"] = "CowboyPewPew/vendor/imgui/include"
+
+include "CowboyPewPew/vendor/glfw_premake5.lua"
+include "CowboyPewPew/vendor/Glad"
 
 project "CowboyPewPew"
 	location "CowboyPewPew"
@@ -23,8 +31,8 @@ project "CowboyPewPew"
 	cppdialect "C++20"
 	staticruntime "on"
 
-    targetdir ("bin/" .. Outputdir .. "/%{prj.name}")
-    objdir ("bin-int/" .. Outputdir .. "/%{prj.name}")
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
     pchheader "pch.h"
     pchsource "CowboyPewPew/src/pch.cpp"
@@ -38,6 +46,15 @@ project "CowboyPewPew"
     includedirs
     {
         "%{prj.name}/src",
+        "%{IncludeDir.GLFW}",
+        "%{IncludeDir.Glad}",
+    }
+
+    links 
+    {
+        "GLFW",
+        "Glad",
+        "opengl32.lib"
     }
 
     filter "system:windows"
