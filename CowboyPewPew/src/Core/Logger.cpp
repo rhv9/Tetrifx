83be30ebbfedaee4f9140a2e867f53dab55c9216
rhv9 <rahulhasmukv@gmail.com>
2024-06-13 21:31:48 +0100

    Minor logging cleanup.

--- CowboyPewPew/src/Core/Logger.cpp
+++ CowboyPewPew/src/Core/Logger.cpp
@@ -11,17 +11,17 @@ void Log::Init()
 {
 	std::vector<spdlog::sink_ptr> logSinks;
 	logSinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
-	logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("Hazel.log", true));
+	logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("cowboy3i.log", true));
 
 	logSinks[0]->set_pattern("%^[%T] %n: %v%$");
 	logSinks[1]->set_pattern("[%T] [%l] %n: %v");
 
-	s_CoreLogger = std::make_shared<spdlog::logger>("GONK", begin(logSinks), end(logSinks));
+	s_CoreLogger = std::make_shared<spdlog::logger>("CORE", begin(logSinks), end(logSinks));
 	spdlog::register_logger(s_CoreLogger);
 	s_CoreLogger->set_level(spdlog::level::trace);
 	s_CoreLogger->flush_on(spdlog::level::trace);
 
-	s_ClientLogger = std::make_shared<spdlog::logger>("APP", begin(logSinks), end(logSinks));
+	s_ClientLogger = std::make_shared<spdlog::logger>("GAME", begin(logSinks), end(logSinks));
 	spdlog::register_logger(s_ClientLogger);
 	s_ClientLogger->set_level(spdlog::level::trace);
 	s_ClientLogger->flush_on(spdlog::level::trace);
